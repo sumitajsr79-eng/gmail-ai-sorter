8b27f950384973b97bd0607db4fb70c80aefaa0b
BeLuFa Baibhav <sumitajsr79@gmail.com>
2026-08-08 12:05:01 +0530

    Add full Android app project (android/) and GitHub Actions automated APK build workflow (.github/workflows/build-apk.yml)

---
+++ android/app/src/main/java/com/smartmail/ai/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.smartmail.ai
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return false
+            }
+        }
+
+        // Points to active SmartMail AI server or local host
+        webView.loadUrl("http://10.0.2.2:5000")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
